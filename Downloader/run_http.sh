257f5d699820f41eeb23ba1140af8a9def2bce3d
Scott <liysy020@gmail.com>
2025-07-09 22:30:56 +1000

    Add files via upload

---
+++ Downloader/run_http.sh
@@ -0,0 +1,7 @@
+AUTOMATION_DIR="/Automation"
+
+until ping -c1 google.com &>/dev/null; do
+  sleep 1
+done
+source "$AUTOMATION_DIR/bin/activate"
+python3 "$AUTOMATION_DIR/manage.py" runserver 127.0.0.1:8000

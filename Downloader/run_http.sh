e4aa1496f419214b5e657578734a3b369b3e5065
Scott <liysy020@gmail.com>
2025-07-26 23:37:26 +1000

    Delete Downloader/run_http.sh

--- Downloader/run_http.sh
+++
@@ -1,7 +0,0 @@
-AUTOMATION_DIR="/Automation"
-
-until ping -c1 google.com &>/dev/null; do
-  sleep 1
-done
-source "$AUTOMATION_DIR/bin/activate"
-nohup python3 "$AUTOMATION_DIR/manage.py" runserver 127.0.0.1:8000

d378508986bb395a57ccd417c1eb15084d8ee7ea
Scott <liysy020@gmail.com>
2025-07-26 23:38:13 +1000

    Update settings.py

--- Downloader/Downloader/settings.py
+++ Downloader/Downloader/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sslserver',
     'Efile',
     'Login',
     'Youtube_downloader',
